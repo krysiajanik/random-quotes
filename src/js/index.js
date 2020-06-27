6037151cb923f2468ed404b0dccc65a14c8b020c
krysiajanik <a.krystyna.janik@gmail.com>
2020-06-27 16:47:40 +0200

    feat: js code added, incl randomization and event listeners

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,38 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import { quotes } from "./quotes.js";
+
+import { registerSW } from "./pwa.js";
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+// Get random number to match index from quotes array
+
+let quotesTotal = quotes.length;
+
+function getRandomQuote(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+//  Get the two elements that need to be changed:
+
+const quoteText = document.querySelector(".quote__text--js");
+const quoteAuthor = document.querySelector(".quote__author--js");
+
+
+// Get the first quote when page is loading
+let fisrtVar = getRandomQuote(quotesTotal);
+quoteText.innerHTML = quotes[fisrtVar].text;
+quoteAuthor.innerHTML = quotes[fisrtVar].author;
+
+const button = document.querySelector(".button--js");
+
+button.addEventListener("click", () => {
+  let myVar = getRandomQuote(quotesTotal);
+  console.log(myVar);
+  quoteText.innerHTML = quotes[myVar].text;
+  quoteAuthor.innerHTML = quotes[myVar].author;
+});
+
+console.log('test')
---
+++ src/js/quotes.js
@@ -0,0 +1,20 @@
+export const quotes = [
+    {author: 'Rachel',
+    text: '“It’s like all of my life, everyone has always told me, ‘You’re a shoe, you’re a shoe, you’re a shoe, you’re a shoe.’ And then today, I just stopped and I said, ‘What if I don’t want to be a shoe? What if I want to be a purse, you know? Or a hat?”'
+    },
+    {author: 'Ross',
+    text: '“Pivot! Pivot! Pivot!”'
+    },
+    {author: 'Joey',
+    text: '“A moo point?” “You know, like a cow’s opinion. It doesn’t matter. It’s moo.”'
+    },
+    {author: 'Phoebe',
+    text: '“Do you have a plan?” “I don’t even have a pla—”'
+    },
+    {author: 'Monica',
+    text: '“Judge all you want, but married a lesbian, left a man at the altar, fell in love with a gay ice dancer, threw a girl’s wooden leg in the fire, lives in a box.” '
+    },
+    {author: 'Chandler',
+    text: '“I’m not so good with the advice….Can I interest you in a sarcastic comment?”'
+    },
+]
